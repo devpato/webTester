c079331e80b2740a3332847caca4b709f4a29052
devpato <pevargas2730@gmail.com>
2020-05-03 16:43:30 -0400

    colors enum created

---
+++ src/js/enums/colors.js
@@ -0,0 +1,15 @@
+/**
+ * Common Colors types used in the peformanceReport class
+ * @typedef accountPerformance
+ * @property {string} GREEN
+ * @property {string} ORANGE
+ * @property {string} RED
+ * @readonly
+ * @enum string
+ */
+
+export const COLOR = Object.freeze({
+    GREEN: "#4CAF50",
+    ORANGE: "#FFAE1A",
+    RED: "#DC143C",
+});
