3087d3591e9829c4e162fcc0368a205a50fa5052
devpato <pevargas2730@gmail.com>
2020-05-10 02:21:00 -0400

    speed enum

---
+++ src/js/enums/speed.js
@@ -0,0 +1,17 @@
+/**
+ * Common Colors types used in the pageReport class (speed)
+ * @typedef accountPerformance
+ * @property {string} FAST
+ * @property {string} AVERAGE
+ * @property {string} SLOW
+ * @property {string} NONE
+ * @readonly
+ * @enum string
+ */
+
+export const SPEED = Object.freeze({
+    FAST: "#4CAF50",
+    AVERAGE: "#FFAE1A",
+    SLOW: '#FF6961',
+    NONE: "#DC143C",
+});
